c37f1ffb2b3a82fc277c0b4ed38dc14e86e6637e
Jamaludin Saputra <jamaludinsaputraaa@gmail.com>
2022-03-07 23:03:56 +0700

    pemanggilan data json ke dalam tabel

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-
 function getData(url, cb) {
-    let xhr = new XMLHttpRequest();
-   
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        return cb(JSON.parse(xhr.responseText));
-      }
-     
-       };
-        xhr.open("GET", url);
-        xhr.send();
-       
-      }
-      let table = "";
-      table += `("<tr>","https://jsonplaceholder.typicode.com/users" )`;
-      table.innerHTML=""
-  const data = getData("https://jsonplaceholder.typicode.com/users", function(data) {
-      console.log(data);
-       
-  });
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      return cb(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send();
+}
+
+  const data = getData("https://jsonplaceholder.typicode.com/users/", function(data) {
   
+  let tbody = document.getElementById("tbody");
+
+    data.map((d)=>{
+     table=`
+         <tr> 
+        <td> ${d.id} </td> 
+        <td> ${d.name} </td> 
+        <td> ${d.username} </td> 
+        <td> ${d.email} </td> 
+        <td> ${d.address.street} ${d.address.suite} ${d.address.city}
+         ${d.address.zipcode} ${d.address.geo.lat} ${d.address.geo.lng}  </td>
+        <td> ${d.company.name} ${d.company.catchPhrase}  ${d.company.bs}       </td>
+        </tr> `
+        tbody.innerHTML+= table;
+    });
+  });
